213a9b596901b0cbb0c51d0af80992b9e0c8efba
Glenn Skawski <57416284+gskawski@users.noreply.github.com>
2022-06-14 07:31:59 -0700

    Create DoublePtr.c

---
+++ DoublePtr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+void allocateMemory( int ** ptr) {
+   *ptr = (int *) malloc(sizeof(int)); // allocate some memory
+}
+
+int main(){
+   int *ptr = NULL;
+   
+   // &ptr = **ptr
+   allocateMemory(&ptr);
+   *ptr = 20;
+   printf("%d\n", *ptr);
+
+   // free up the memory
+   free(ptr);
+   return 0;
+}
