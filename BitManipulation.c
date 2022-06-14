2130ac593014a872983bb85eb543b86945fbb141
Glenn Skawski <57416284+gskawski@users.noreply.github.com>
2022-06-14 08:05:58 -0700

    Create BitManipulation.c

---
+++ BitManipulation.c
@@ -0,0 +1,10 @@
+s = sequence
+0110 + 0110 = 0110 * 2 = 0110 << 1
+a ^ (~a) = 1s
+~0 = 1s
+// arithmetic right shift
+a >> 4 = a / 16
+// logical right shift
+ -75 >>> 1 = 90
+ 10110101 >>> 1 =
+ 01011010
